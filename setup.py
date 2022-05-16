b5f35ee3b2a9220625676b37ea31e28a4524f062
perspe <m.alexandre@campus.fct.unl.pt>
2022-05-16 22:53:48 +0100

    Updated config files to consider pyx files

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import setup
 from distutils.core import Extension
-from Cython.Build import cythonize
 import os
 
+from Cython.Build import cythonize
+import numpy as np
+from setuptools import setup
+
 # File shortcuts
+# SMM
 smm_core = os.path.join("em_methods", "smm_core")
 py_smm_base = os.path.join(smm_core, "py_smm_base.pyx")
 cpp_smm_base = os.path.join(smm_core, "smm_base.cpp")
+# RCWA
+rcwa = os.path.join("em_methods", "rcwa_core")
+py_rcwa_base = os.path.join(rcwa, "py_rcwa_core.pyx")
 
 ext = [
     Extension("em_methods.smm_core.py_smm_base", [py_smm_base, cpp_smm_base],
-              language="c++",
-              include_dirs=[smm_core])
+              include_dirs=[smm_core, np.get_include()]),
+    Extension("em_methods.rcwa_core.py_rcwa_core", [py_rcwa_base],
+              include_dirs=[np.get_include()])
 ]
 
 setup(ext_modules=cythonize(ext))
