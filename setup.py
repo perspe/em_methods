c6172ad7698c7b170b528ea8e26d3b0691b2cabe
perspe <m.alexandre@campus.fct.unl.pt>
2022-07-02 21:10:49 +0100

    Cleanup old rcwa stuff

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ smm_core = os.path.join("em_methods", "smm_core")
 py_smm_base = os.path.join(smm_core, "py_smm_base.pyx")
 cpp_smm_base = os.path.join(smm_core, "smm_base.cpp")
 # RCWA
-rcwa = os.path.join("em_methods", "rcwa_core")
-py_rcwa_base = os.path.join(rcwa, "rcwa_core.pyx")
+# rcwa = os.path.join("em_methods", "rcwa_core")
+# py_rcwa_base = os.path.join(rcwa, "rcwa_core.pyx")
 
 ext = [
     Extension("em_methods.smm_core.py_smm_base", [py_smm_base, cpp_smm_base],
               include_dirs=[smm_core, np.get_include()]),
-    Extension("em_methods.rcwa_core.rcwa_core", [py_rcwa_base],
-              include_dirs=[np.get_include()])
+    # Extension("em_methods.rcwa_core.rcwa_core", [py_rcwa_base],
+    #           include_dirs=[np.get_include()])
 ]
 
 setup(ext_modules=cythonize(ext))
