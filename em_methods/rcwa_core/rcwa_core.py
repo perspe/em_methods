502a51e831f01608f81a6893c20b453668ca3fbe
perspe <m.alexandre@campus.fct.unl.pt>
2022-07-06 14:32:25 +0100

    (feat): Added parameter initialization/reflected field calculation

--- em_methods/rcwa_core/rcwa_core.py
+++ em_methods/rcwa_core/rcwa_core.py
@@ -2,11 +2,17 @@ import logging
 import numpy as np
 import numpy.typing as npt
 from scipy.linalg import inv, eig
+from typing import Tuple
+from em_methods.grid import Grid2D
+import os
 
 from logging.config import fileConfig
 
-fileConfig('../logging.ini')
+# Get module logger
+base_path = os.path.dirname(os.path.abspath(__file__))
+fileConfig(os.path.join(base_path, '..', 'logging.ini'))
 logger = logging.getLogger('dev')
+""" Matrices for the Scattering Matrix Connection """
 
 
 class SMBase:
@@ -19,13 +25,13 @@ class SMBase:
     __slots__ = ('_S11', '_S12', '_S21', '_S22', '_V', '_W')
 
     def __init__(self, m: int, n: int) -> None:
-        self._S11 = np.zeros((m, n), order="F")
-        self._S12 = np.eye(m, n, order="F")
-        self._S21 = np.eye(m, n, order="F")
-        self._S22 = np.zeros((m, n), order="F")
+        self._S11 = np.zeros((m, n))
+        self._S12 = np.eye(m, n)
+        self._S21 = np.eye(m, n)
+        self._S22 = np.zeros((m, n))
 
     def __mul__(self, SB):
-        eye = np.eye(self._S11.shape[0], self._S11.shape[1], order="F")
+        eye = np.eye(self._S11.shape[0], self._S11.shape[1])
         redhaf_matrix = SMBase(self._S11.shape[0], self._S11.shape[1])
         inv_1 = self._S12 @ inv(eye - SB._S11 @ self._S22, check_finite=False)
         inv_2 = SB._S21 @ inv(eye - self._S22 @ SB._S11, check_finite=False)
@@ -43,7 +49,7 @@ class SMBase:
 class SFree(SMBase):
     """ Scattering Matrix for the Free Space Region """
     def __init__(self, Kx: npt.NDArray, Ky: npt.NDArray) -> None:
-        eye = np.eye(Kx.shape[0], Kx.shape[1], order="F")
+        eye = np.eye(Kx.shape[0], Kx.shape[1])
         P = np.block([[Kx @ Ky, eye - Kx @ Kx], [Ky @ Ky - eye, -Kx @ Ky]])
         eigval, self._W = eig(P @ P, check_finite=False)
         eig_matrix = np.diag(1 / np.sqrt(eigval))
@@ -55,7 +61,7 @@ class SRef(SMBase):
     """ Reflection Region Scattering Matrix """
     def __init__(self, Kx: npt.NDArray, Ky: npt.NDArray, e_ref: complex,
                  u_ref: complex, sfree: SFree) -> None:
-        eye = np.eye(Kx.shape[0], Kx.shape[1], order="F")
+        eye = np.eye(Kx.shape[0], Kx.shape[1])
         matrix = np.block([[Kx @ Ky, e_ref * u_ref * eye - Kx @ Kx],
                            [Ky @ Ky - e_ref * u_ref * eye, -Kx @ Ky]])
         P, Q = matrix / e_ref, matrix / u_ref
@@ -83,7 +89,7 @@ class STrn(SMBase):
     """ Transmission Region Scattering Matrix """
     def __init__(self, Kx: npt.NDArray, Ky: npt.NDArray, e_trn: complex,
                  u_trn: complex, sfree: SFree) -> None:
-        eye = np.eye(Kx.shape[0], Kx.shape[1], order="F")
+        eye = np.eye(Kx.shape[0], Kx.shape[1])
         matrix = np.block([[Kx @ Ky, e_trn * u_trn * eye - Kx @ Kx],
                            [Ky @ Ky - e_trn * u_trn * eye, -Kx @ Ky]])
         P, Q = matrix / e_trn, matrix / u_trn
@@ -117,7 +123,7 @@ class SMatrix(SMBase):
                       [Ky @ inv_er @ Ky - u0, -Ky @ inv_er @ Kx]])
         Q = np.block([[Kx @ inv_u0 @ Ky, er - Kx @ inv_u0 @ Kx],
                       [Ky @ inv_u0 @ Ky - er, -Ky @ inv_u0 @ Kx]])
-        self._W = np.array(P @ Q, dtype=np.complex128, order="F")
+        self._W = np.array(P @ Q, dtype=np.complex128)
         eigs, self._W = eig(self._W, check_finite=False)
         logger.debug(f"self._W =\n{self._W}")
         inv_W = inv(self._W, check_finite=False)
@@ -144,31 +150,69 @@ class SMatrix(SMBase):
         logger.debug(self)
 
 
-""" Test Section """
+""" Intermediary functions for calculations """
 
 
-def test_redhaff():
-    size = 1
-    kx = np.diag(np.linspace(0, 1, size))
-    ky = np.diag(np.linspace(0, 1, size))
-    kx = np.array(kx, order="F")
-    ky = np.array(ky, order="F")
-    e = np.eye(size, size, dtype=np.complex128, order="F")
-    logger.debug(f"Input Arrays:\n{kx}")
-    sfree = SFree(kx, ky)
-    logger.debug("---------------- Layer 1 -------------------------")
-    SM = SMatrix(kx, ky, 5.0449 * e, e, sfree, 314.1593, 0.005)
-    logger.debug("---------------- Layer 2 -------------------------")
-    SM2 = SMatrix(kx, ky, 6 * e, e, sfree, 314.1593, 0.003)
-    logger.debug("---------------- Redhaff -------------------------")
-    sdev = SM * SM2
-    logger.debug("---------------- SRef -------------------------")
-    sref = SRef(kx, ky, 2, 1, sfree)
-    logger.debug("---------------- STrn -------------------------")
-    strn = STrn(kx, ky, 9, 1, sfree)
-    _ = sref * sdev * strn
+def r_t_fields(s_global: SMBase, s_ref: SMBase, s_trn: SMBase,
+               e_src: npt.NDArray, Kx: npt.NDArray, Ky: npt.NDArray,
+               Kz_ref: npt.NDArray,
+               Kz_trn: npt.NDArray) -> Tuple[complex, complex]:
+    """ Calculate the Reflected and Transmited fields """
+    # Calculate reflected and transmited fields
+    c_src = inv(s_ref._W) @ e_src
+    e_ref = s_ref._W @ s_global._S11 @ c_src
+    e_trn = s_trn._W @ s_global._S21 @ c_src
+    logger.debug(f"e_ref: {e_ref}")
+    logger.debug(f"e_trn: {e_trn}")
+    # Compute the longitudinal components
+    e_ref_x, e_ref_y = e_ref[:int(e_ref.size / 2)], e_ref[int(e_ref.size / 2):]
+    e_trn_x, e_trn_y = e_trn[:int(e_trn.size / 2)], e_trn[int(e_trn.size / 2):]
+    rz = -inv(Kz_ref) @ (Kx @ e_ref_x + Ky @ e_ref_y)
+    tz = -inv(Kz_trn) @ (Kx @ e_trn_x + Ky @ e_trn_y)
+    logger.debug(f"[{e_ref_x} {e_ref_y} {rz}]::[{e_trn_x} {e_trn_y} {tz}]")
+    r2 = np.sum(np.abs([e_ref_x, e_ref_y, rz])**2)
+    t2 = np.sum(np.abs([e_trn_x, e_trn_y, tz])**2)
+    return r2, t2
 
 
-
-if __name__ == "__main__":
-    test_redhaff()
+def initialize_components(theta: float, phi: float, lmb: float,
+                          pol: Tuple[complex, complex], p: int, q: int,
+                          dx: float, dy: float, inc_med: Tuple[complex,
+                                                               complex],
+                          trn_med: Tuple[complex, complex]):
+    """ Initialize all the components necessary for the RCWA Algorithm """
+    logger.debug("Initialization values for SMatrix")
+    k0: float = 2 * np.pi / lmb
+    logger.debug(f"{k0=}")
+    e_ref, u_ref = inc_med
+    e_trn, u_trn = trn_med
+    # Determine the incidence k vector
+    kx_inc: float = np.sqrt(e_ref * u_ref) * np.sin(theta) * np.cos(phi)
+    ky_inc: float = np.sqrt(e_ref * u_ref) * np.sin(theta) * np.sin(phi)
+    kz_inc: float = np.sqrt(u_ref*e_ref - k0*(kx_inc**2 + ky_inc**2))
+    logger.debug(f"k_inc: [{kx_inc} {ky_inc} {kz_inc}]")
+    # Determine the wavevector matrices from the number of harmonics
+    kx_p = kx_inc - np.arange(-p + 1, p) * 2 * np.pi / dx
+    ky_q = ky_inc - np.arange(-q + 1, q) * 2 * np.pi / dy
+    kz_ref = np.conjugate(np.sqrt(k0**2 * u_ref * e_ref - kx_p**2 - ky_q**2))
+    kz_trn = np.conjugate(np.sqrt(k0**2 * u_trn * e_trn - kx_p**2 - ky_q**2))
+    Kx = np.diag(kx_p) / k0
+    Ky = np.diag(ky_q) / k0
+    Kz_ref = np.diag(kz_ref) / k0
+    Kz_trn = np.diag(kz_trn) / k0
+    # Determine the Free Space Scattering Matrix
+    sfree = SFree(Kx, Ky)
+    # Reduced polarization vector
+    if theta == 0:
+        ate = np.array([0, 1, 0])
+        atm = np.array([1, 0, 0])
+    else:
+        ate = np.array([-np.sin(phi), np.cos(phi), 0])
+        atm = np.array([
+            np.cos(phi) * np.cos(theta),
+            np.cos(theta) * np.sin(phi), -np.sin(theta)
+        ])
+    # Create the composite polariztion vector
+    p_vector = np.add(pol[1] * ate, pol[0] * atm)
+    p = p_vector[[0, 1]]
+    return k0, sfree, Kx, Ky, Kz_ref, Kz_trn, p
