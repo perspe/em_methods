007797c689ee3365011946c726826e8cece12ef1
Miguel Alexandre <m.alexandre@campus.fct.unl.pt>
2023-02-02 10:11:34 +0000

    Improved function to calculate jsc

--- em_methods/utilities.py
+++ em_methods/utilities.py
@@ -13,8 +13,16 @@ data_path = os.path.join(parent_path, "data")
 
 
 def jsc_files(filename: Union[List[str], str],
+              percent: bool = False,
               **read_csv_args) -> Union[pd.Series, float]:
-    """ Calculate the Jsc for filename """
+    """
+    Calculate Jsc for a single or multiple files
+    Args:
+        filename: file or list of files
+        percent: EQE or absorption files in %
+        **read_csv_args: pass args for read_csv
+    """
+    percent_factor: int = 100 if percent else 1
     # Import spectrum and interpolate
     solar_spectrum = pd.read_csv(os.path.join(data_path, "solar_data.csv"),
                                  sep=" ",
@@ -26,16 +34,16 @@ def jsc_files(filename: Union[List[str], str],
         results: Dict = {}
         for file in filename:
             data = pd.read_csv(file, **read_csv_args)
-            wvl = data.iloc[:, 0]
-            abs = data.iloc[:, 1]
+            wvl = data.iloc[:, 0].values
+            abs = data.iloc[:, 1].values/percent_factor
             # Make the integration
             results[os.path.basename(file)] = trapz(
                 int_spectrum(wvl) * abs * wvl / factor, wvl)
         return pd.Series(results)
     else:
         data = pd.read_csv(filename, **read_csv_args)
-        wvl = data.iloc[:, 0]
-        abs = data.iloc[:, 1]
+        wvl = data.iloc[:, 0].values
+        abs = data.iloc[:, 1].values/percent_factor
         # Make the integration
         Jsc = trapz(int_spectrum(wvl) * abs * wvl / factor, wvl)
         return Jsc
