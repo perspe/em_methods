47ff6bc99575e32cd5ead3858b721d9606b61a05
Miguel Alexandre <m.alexandre@campus.fct.unl.pt>
2024-05-29 14:55:05 +0100

    (test.test_solar_power_angle) More test functions

--- test/test_solar_power_angle.py
+++ test/test_solar_power_angle.py
@@ -1,15 +1,21 @@
-import unittest
 import datetime
 from datetime import datetime
-from pytz import timezone
 import logging
+import unittest
+
+import matplotlib.pyplot as plt
+import numpy as np
+from pytz import timezone
 
 from em_methods.pv import solar_angle, solar_power
+from em_methods.pv import eot_main, solar_declination
 
 # Override logger to always use debug
 logger = logging.getLogger("sim")
 logger.setLevel(logging.DEBUG)
 # BASETESTPATH: str = os.path.join("test", "pv")
+
+PLOTS=True
 
 class TestIrr(unittest.TestCase):
     def test_run(self):
@@ -37,7 +43,21 @@ class TestIrr(unittest.TestCase):
         Determine solar power irrandiance in location (based on Cristina results)
         """
         date = datetime(2024, 6, 21, 13, 0, 0)
-        pin, pdirect, pmodule = solar_power(-9.142685, 38.736946, 0.002, 35, date)
-        # self.assertAlmostEqual(zenith, 67.6, 1)
-        # self.assertAlmostEqual(azimuth, 221.2, 1)
+        ghi, gti, gain = solar_power(-9.142685, 38.736946, 0.002, 35, date)
+        self.assertAlmostEqual(ghi, 0.992, 3)
+        self.assertAlmostEqual(gti, 0.968, 3)
+        self.assertAlmostEqual(gain, -2.5, 1)
 
+    @unittest.skipIf(PLOTS==False, "Test defined to not show plots")
+    def test_internal_functions(self):
+        """
+        Test various internal functions
+        """
+        days = np.linspace(0, 365, 365, dtype=np.integer)
+        eots = eot_main(days)
+        declinations = solar_declination(days)
+        plt.plot(days, eots, label="EoT")
+        plt.plot(days, declinations, label="Solar Declination")
+        plt.xlabel("Days")
+        plt.legend()
+        plt.show()
