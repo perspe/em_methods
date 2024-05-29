980ca2c3958996a8cfd1eaaafcf8d1820317baf9
Miguel Alexandre <m.alexandre@campus.fct.unl.pt>
2024-05-29 14:54:30 +0100

    (test.solar_power_angle) New minor functions

--- em_methods/pv/solar_power_angle.py
+++ em_methods/pv/solar_power_angle.py
@@ -1,13 +1,70 @@
 import datetime
 import logging
 import math
+from typing import Union
 
 import numpy as np
+import numpy.typing as npt
 from pytz import timezone
 import scipy.constants as scc
 
 # Get module logger
 logger = logging.getLogger("sim")
+
+
+def solar_declination(
+    days_start_year: Union[npt.NDArray[np.integer], int]
+) -> Union[int, npt.NDArray[np.floating]]:
+    """
+    Calculate solar declination
+    """
+    gamma = 2 * scc.pi * (days_start_year - 81) / 365
+    declination = 23.45 * np.sin(gamma)
+    return declination
+
+
+def eot_main(
+    days_start_year: Union[npt.NDArray[np.integer], int]
+) -> Union[float, npt.NDArray[np.floating]]:
+    gamma = 2 * scc.pi * (days_start_year - 81) / 365
+    eot = 9.87 * np.sin(2 * gamma) - 7.53 * np.cos(gamma) - 1.5 * np.sin(gamma)
+    return eot
+
+
+def eot1(dt: datetime.datetime, days_start_year: int) -> Union[float, npt.NDArray]:
+    """
+    Old formula to calculate EoT
+    """
+    gamma = 2 * scc.pi / 365 * (days_start_year - 1 + (dt.hour - 12) / 24)
+    eot = 229.18 * (
+        0.000075
+        + 0.001868 * math.cos(gamma)
+        - 0.032077 * math.sin(gamma)
+        - 0.014615 * math.cos(2 * gamma)
+        - 0.040849 * math.sin(2 * gamma)
+    )
+    return eot
+
+
+def am_kasten(zenith: Union[float, npt.NDArray]) -> Union[float, npt.NDArray]:
+    """
+    Calculate solar power from Kasten formula
+    """
+    # Calculate Power (Kasten and Young)
+    zenith_rad = np.radians(zenith)
+    air_mass = 1 / (math.cos(zenith_rad) + 0.50572 * (96.07995 - zenith) ** (-1.6364))
+    return air_mass
+
+
+def am_rozenberg(zenith: Union[float, npt.NDArray]) -> Union[float, npt.NDArray]:
+    """
+    Calculate solar power from the Rozenberg formula
+    """
+    zenith_rad = np.radians(zenith)
+    air_mass = (
+        np.cos(zenith_rad) + 0.025 * np.exp(-11 * np.cos(math.radians(zenith)))
+    ) ** -1
+    return air_mass
 
 
 def solar_angle(
@@ -31,20 +88,11 @@ def solar_angle(
     time_diff = dt.strftime("%z")
     time_diff = int(time_diff[1:2]) + int(time_diff[2:]) / 60
     lstm = 15 * time_diff
-    gamma = 2 * scc.pi * (days_start_year - 81) / 365
-    eq_time = (
-        9.87 * math.sin(2 * gamma) - 7.53 * math.cos(gamma) - 1.5 * math.sin(gamma)
-    )  # Equation of Time (EoT) in minutes
-    # Equations of v1
-    # gamma = 2 * scc.pi / 365 * (days_start_year - 1 + (dt.hour - 12) / 24)
-    # eq_time = 229.18 * (
-    #     0.000075
-    #     + 0.001868 * math.cos(gamma)
-    #     - 0.032077 * math.sin(gamma)
-    #     - 0.014615 * math.cos(2 * gamma)
-    #     - 0.040849 * math.sin(2 * gamma)
-    # )
-    tc = 4 * (longitude - lstm) + eq_time
+    eot = eot_main(days_start_year)
+    declination = solar_declination(days_start_year)
+    if not isinstance(eot, float) or not isinstance(declination, float):
+        raise TypeError("EoT or declination have wrong type")
+    tc = 4 * (longitude - lstm) + eot
     lst = dt + datetime.timedelta(minutes=tc)
     lst_offset_h = lst + datetime.timedelta(hours=-12)
     lst_offset_h = lst_offset_h.hour + lst_offset_h.minute / 60
@@ -55,15 +103,13 @@ Initial date: {dt}
 Day of the year: {days_start_year}
 Time difference (Timezone): {time_diff}
 LSTM: {lstm}
-EoT: {eq_time}
+EoT: {eot}
 TC: {tc}
 LST_offset: {lst_offset_h}
 LST: {lst}
 Hour angle in local solar time: {hra}
 """
     )
-    # Determine the solar angles and the sunrise/sunset times
-    declination = 23.45 * math.sin(gamma)
     # Convert delta, hra and latitude to radians
     declination, hra, latitude = tuple(
         np.radians(var) for var in [declination, hra, latitude]
@@ -122,11 +168,10 @@ def solar_power(
     """
     zenith, azimuth, *_ = solar_angle(longitude, latitude, dt, default_tz)
     zenith_rad, azimuth_rad = math.radians(zenith), math.radians(azimuth)
+    air_mass = am_kasten(zenith)
     tilt_rad = math.radians(tilt)
     elevation = scc.pi / 2 - zenith_rad
     azimuth_pannel = scc.pi if latitude > 0 else 0
-    # Calculate Power (Kasten and Young)
-    air_mass = 1 / (math.cos(zenith_rad) + 0.50572 * (96.07995 - zenith) ** (-1.6364))
     # Direct Normal irradiance
     dni = 1.353 * ((1 - 0.14 * height) * (0.7 ** (air_mass**0.678)) + 0.14 * height)
     # Global horizontal irradiance
@@ -136,7 +181,9 @@ def solar_power(
         1.1
         * dni
         * (
-            math.cos(elevation) * math.sin(tilt_rad) * math.cos(azimuth_pannel - azimuth_rad)
+            math.cos(elevation)
+            * math.sin(tilt_rad)
+            * math.cos(azimuth_pannel - azimuth_rad)
             + math.sin(elevation) * math.cos(tilt_rad)
         )
     )
