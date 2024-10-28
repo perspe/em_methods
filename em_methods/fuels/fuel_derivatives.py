f726fe3dc39c78e4fd3bec4ee92c2fafe36fa1f1
perspe <m.alexandre@campus.fct.unl.pt>
2024-10-28 13:39:47 +0000

    (More fuel)

--- em_methods/fuels/fuel_derivatives.py
+++ em_methods/fuels/fuel_derivatives.py
@@ -1,16 +1,55 @@
-from typing import List
+from typing import List, Union
+import scipy.constants as scc
+import numpy.typing as npt
+import numpy as np
 
 city: str = "Sines"
 
 # Constants
 MW_CO2: float = 44.01  # g/mol
 MW_H2: float = 2.01568  # g/mol
+WH_TO_MJ = 0.0036
 
 H2_KG_CITY = {
     "Sines": {"constant": 27.34758880209588, "variable": 23.8878},
     "Edmonton": {"constant": 19.64191460924014, "variable": 17.7885},
     "Crystal Brook": {"constant": 25.081214039491254, "variable": 22.2923},
 }
+
+
+def hydrogen_mass_to_volume(
+    mass: Union[npt.NDArray, float],
+    temp: Union[npt.NDArray, float],
+    pressure: Union[npt.NDArray, float],
+) -> Union[npt.NDArray, float]:
+    """
+    Convert hydrogen mass (kg) to mass (m3)
+    Args:
+        mass: Hydrogen volume in kg
+        temp: Temperature (in K)
+        pressure: Pressure (in Pa)
+    """
+    molar_mass_h2 = 0.002016
+    n = mass * scc.R * temp / pressure
+    return n / molar_mass_h2
+
+
+def hydrogen_volume_to_mass(
+    volume: Union[npt.NDArray, float],
+    temp: Union[npt.NDArray, float],
+    pressure: Union[npt.NDArray, float],
+) -> Union[npt.NDArray, float]:
+    """
+    Convert hydrogen volume (m3) to mass (kg)
+    Args:
+        volume: Hydrogen volume in m3
+        temp: Temperature (in K)
+        pressure: Pressure (in Pa)
+    """
+    molar_mass_h2 = 0.002016
+    n = (pressure * volume) / (scc.R * temp)
+    return n * molar_mass_h2
+
 
 def fuel_properties(
     heating_value: float,
@@ -58,3 +97,52 @@ def fuel_properties(
         revenue_after_tax,
         loss_tax,
     ]
+
+
+def electrochemical(
+    h2_production: Union[npt.NDArray, float],
+    ec_consumed: Union[npt.NDArray, float],
+    water_consumed: Union[npt.NDArray, float],
+    system_losses: Union[npt.NDArray, float],
+    h2_heating_value: Union[npt.NDArray, float],
+    pv_yield: Union[npt.NDArray, float],
+    ec_capacity: float = 0.5,
+    units: str = "relative",
+    temp: float = 298.15,
+    pressure: float = scc.atm,
+):
+    """
+    Args:
+        h2_production (Nm3/h): volume of H2 produced per hour
+        ec_consumed (Wh/h): energy consumed by the EC per hour
+        water_consumed (L/Wh): water consumed per Wh of energy consumed
+        system_losses (%): EC system losses in the electronic parts
+        h2_heating_value (MJ/kg): lower heating value of hydrogen, aka energy density
+        pv_yield (same as 'units'): PV energy yield considering temperature, reflection and system losses per hour
+        ec_capacity (Wh/h): Capacity of each electrochemical cell
+        units ('relative' or 'absolute'): W/Wpeak or W
+    """
+    if units == "relative":
+        capacity = ec_capacity
+    elif units == "absolute":
+        capacity = ec_capacity * 1e6
+    else:
+        raise Exception("Invalid value in 'units' (use 'relative' or 'absolute'")
+    consumption_losses_ph = ec_consumed * (1 + system_losses / 100)
+    ec_units = ec_capacity / ec_consumed
+    # Ratio between energy supplied and required
+    supply_ratio = pv_yield / (ec_units * consumption_losses_ph)
+    supply_ratio = np.clip(supply_ratio, 0, 1)
+    supply_energy = ec_units * ec_consumed * supply_ratio
+    # H2 production
+    h2_volume = ec_units * h2_production * supply_ratio
+    water_consumption = supply_energy * water_consumed * 1000
+    h2_mass = hydrogen_volume_to_mass(h2_volume, temp, pressure)
+    h2_calorific_power = h2_mass / 1000 * h2_heating_value / WH_TO_MJ
+    return (
+        supply_ratio,
+        supply_energy,
+        h2_mass,
+        h2_calorific_power,
+        water_consumption,
+    )
