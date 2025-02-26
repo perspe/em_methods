813f3afb52dd16cce4f666785629522ea323eb94
Miguel Alexandre <m.alexandre@campus.fct.unl.pt>
2025-02-26 11:51:54 +0000

    Small updates to unittests

--- test/test_charge.py
+++ test/test_charge.py
@@ -2,13 +2,15 @@ import time
 import unittest
 import os
 import logging
-from em_methods.lumerical import charge_run, LumericalError, SimInfo, run_fdtd_and_charge
+from em_methods.lumerical import charge_run, LumericalError, SimInfo, run_fdtd_and_charge, get_gen
 import pandas as pd
 
 # Override logger to always use debug
-logger = logging.getLogger("dev")
+logger = logging.getLogger("sim")
+logger.setLevel(logging.DEBUG)
 
-BASETESTPATH: str = os.path.join("test", "charge")
+BASETESTPATH_CHARGE: str = os.path.join("test", "charge")
+BASETESTPATH_FDTD_CHARGE: str = os.path.join("test", "fdtd_and_charge")
 
 def test_function(charge):
     logger.debug(f"Function runned in {charge}")
@@ -17,30 +19,30 @@ def test_function(charge):
 class TestCHARGE(unittest.TestCase):
     def test_single_run(self):
         """Make a single run of a test file with everything default"""
-        charge_file: str = os.path.join(BASETESTPATH, "teste_planar_2d.ldev")
+        charge_file: str = os.path.join(BASETESTPATH_CHARGE, "teste_planar_2d.ldev")
         properties = {"::model": {"tITO": 0.1e-6, "tSnO2": 0.04e-6, "tSpiro": 0.1e-6}}
-        Si = SimInfo("solar_generation_Si","G_Si.mat", "Si", "AZO", "ITO_bottom")
+        get_results = {"CHARGE": "AZO"}
         try:
             charge_run(
                 charge_file,
                 properties,
-                Si,
-                delete=True,
-                device_kw={"hide": True},)
+                get_results,
+                delete=False,
+                device_kw={"hide": True})
         except LumericalError:
             logger.critical("Error running file")
 
     def test_multiple_run(self):
         """Make a single run of a test file with everything default"""
-        charge_file: str = os.path.join(BASETESTPATH, "teste_planar_2d.ldev")
+        charge_file: str = os.path.join(BASETESTPATH_CHARGE, "teste_planar_2d.ldev")
         properties = {"::model": {"tITO": 0.1e-6, "tSnO2": 0.04e-6, "tSpiro": 0.1e-6}}
-        Si = SimInfo("solar_generation_Si","G_Si.mat", "Si", "AZO", "ITO_bottom")
+        get_results = {"CHARGE": "AZO"}
         for i in range(100):
             try:
                 charge_run(
                     charge_file,
                     properties,
-                    Si,
+                    get_results,
                     delete=True,
                     device_kw={"hide": True},)
             except LumericalError:
@@ -48,12 +50,13 @@ class TestCHARGE(unittest.TestCase):
 
     def test_run_function(self):
         """ Test run with internal function to charge """
-        charge_file: str = os.path.join(BASETESTPATH, "teste_planar_2d.ldev")
+        charge_file: str = os.path.join(BASETESTPATH_CHARGE, "teste_planar_2d.ldev")
         properties = {"::model": {"tITO": 0.1e-6, "tSnO2": 0.04e-6, "tSpiro": 0.1e-6}}
+        get_results = {"CHARGE": "AZO"}
         charge_run(
             charge_file,
             properties,
-            [0, 1, 2, "AZO"],
+            get_results,
             delete=True,
             func=test_function,
             device_kw={"hide": True},
@@ -64,12 +67,13 @@ class TestCHARGE(unittest.TestCase):
     @unittest.expectedFailure
     def test_run_error(self):
         """ Test run file that gives error """
-        charge_file: str = os.path.join(BASETESTPATH, "teste_run_error.ldev")
+        charge_file: str = os.path.join(BASETESTPATH_CHARGE, "teste_run_error.ldev")
         properties = {"::model": {"tITO": 0.1e-6, "tSnO2": 0.04e-6, "tSpiro": 0.1e-6}}
+        get_results = {"CHARGE": "AZO"}
         charge_run(
             charge_file,
             properties,
-            [0, 1, 2, "AZO"],
+            get_results,
             delete=True,
             func=test_function,
             device_kw={"hide": True},
@@ -77,7 +81,7 @@ class TestCHARGE(unittest.TestCase):
 
     def test_version_error(self):
         """ Test run file that gives error """
-        charge_file: str = os.path.join(BASETESTPATH, "teste_version_error.ldev")
+        charge_file: str = os.path.join(BASETESTPATH_CHARGE, "teste_version_error.ldev")
         properties = {"::model": {"tITO": 0.1e-6, "tSnO2": 0.04e-6, "tSpiro": 0.1e-6}}
         try:
             charge_run(
@@ -95,7 +99,7 @@ class TestCHARGE(unittest.TestCase):
 
     def test_get_info(self):
         """Make a single run of a test file with everything default"""
-        charge_file: str = os.path.join(BASETESTPATH, "teste_planar_2d.ldev")
+        charge_file: str = os.path.join(BASETESTPATH_CHARGE, "teste_planar_2d.ldev")
         properties = {"::model": {"tITO": 0.1e-6, "tSnO2": 0.04e-6, "tSpiro": 0.1e-6}}
         info = {"simulation region si": "x span"}
         _, _, _, info = charge_run(
